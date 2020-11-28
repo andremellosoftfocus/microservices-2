c0827ccf7ba68ce7c7fd9500220d8163da5fbfbb
Andre Mello <andre.mello@softfocus.com.br>
2020-11-28 11:45:35 -0300

    jms

---
+++ produtos/src/main/java/com/gasparbarancelli/produtos/api/ProdutoApi.java
@@ -0,0 +1,37 @@
+package com.gasparbarancelli.produtos.api;
+
+import com.gasparbarancelli.produtos.model.Produto;
+import com.gasparbarancelli.produtos.repository.ProdutosRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("")
+public class ProdutoApi {
+
+    @Autowired private ProdutosRepository repository;
+
+    @GetMapping
+    public List<Produto> all() {
+        return repository.findAll();
+    }
+
+    @GetMapping("{id}")
+    public Optional<Produto> one(@PathVariable("id") Long id) {
+        return repository.findById(id);
+    }
+
+    @PostMapping
+    public Produto insert(@RequestBody Produto produto) {
+        return repository.save(produto);
+    }
+
+    @DeleteMapping("{id}")
+    public void delete(@PathVariable("id") Long id) {
+        repository.deleteById(id);
+    }
+
+}
